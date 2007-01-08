bb44735d949ccb0a4b060d7c21c226908b313c99
ryusuke <ryusuke@b46d7009-88a7-42f0-83c0-5976d636eeb3>
2007-01-08 10:08:52 +0000

    Update examples/search.php
    git-svn-id: file:///Volumes/devel/repos/Services_HyperEstraier@18 b46d7009-88a7-42f0-83c0-5976d636eeb3

--- examples/search.php
+++ examples/search.php
@@ -14,36 +14,21 @@ $cond->setSkip(0);
 // get the result of search
 $nres = $node->search($cond, 0);
 if ($nres) {
-    // for each document in the result without iteration
-    /*for ($i = 0; $i < $nres->docNum(); $i++) {
-        // get a result document object
-        $rdoc = $nres->getDocument($i);
-        // display attributes
-        if (($value = $rdoc->getAttribute('@uri')) !== null) {
-            fprintf(STDOUT, "URI: %s\n", $value);
+    if ($nres->docNum() == 0) {
+        fprintf(STDOUT, "%s: not found.\n", $cond->getPhrase());
+    } else {
+        foreach ($nres as $rdoc) {
+            // display attributes
+            if (($value = $rdoc->getAttribute('@uri')) !== null) {
+                fprintf(STDOUT, "URI: %s\n", $value);
+            }
+            if (($value = $rdoc->getAttribute('@title')) !== null) {
+                fprintf(STDOUT, "Title: %s\n", $value);
+            }
+            // display the snippet text (with property overloading)
+            fprintf(STDOUT, "%s", $rdoc->snippet);
         }
-        if (($value = $rdoc->getAttribute('@title')) !== null) {
-            fprintf(STDOUT, "Title: %s\n", $value);
-        }
-        // display the snippet text (with getter method)
-        fprintf(STDOUT, "%s", $rdoc->getSnippet());
-    }*/
-    // for each document in the result as an iterator
-    $j = 0;
-    foreach ($nres as $rdoc) {
-        $j++;
-        // display attributes
-        if (($value = $rdoc->getAttribute('@uri')) !== null) {
-            fprintf(STDOUT, "URI: %s\n", $value);
-        }
-        if (($value = $rdoc->getAttribute('@title')) !== null) {
-            fprintf(STDOUT, "Title: %s\n", $value);
-        }
-        // display the snippet text (with property overloading)
-        fprintf(STDOUT, "%s", $rdoc->snippet);
     }
-    // debug output
-    //var_dump($i, $j, $nres->docNum());
 } else {
     fprintf(STDERR, "error: %d\n", $node->status);
     if (Services_HyperEstraier::getErrorStack()->hasErrors()) {
